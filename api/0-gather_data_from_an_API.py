400f0eede1a62669405b0ab6e520cd069abbe45d
gallit0 <mateogesede@gmail.com>
2022-11-30 16:36:56 -0300

    task 0 new attempt at checker

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,23 +4,23 @@ import json
 import requests
 from sys import argv
 
+src = 'https://jsonplaceholder.typicode.com/'
+
 if __name__ == '__main__':
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    u = requests.get('https://jsonplaceholder.typicode.com/users/' + argv[1])
-    todos_dict = json.loads(todos.text)
-    user_dict = json.loads(u.text)
-    EMPLOYEE_NAME = user_dict['name']
+    todos = requests.get(src + "todos?userId=" + argv[1]).json()
+    u = requests.get(src + 'users/' + argv[1]).json()
+    EMPLOYEE_NAME = u['name']
     NUMBER_OF_DONE_TASKS = 0
     TOTAL_NUMBER_OF_TASKS = 0
     done = []
-    for i in list(todos_dict):
+
+    for i in list(todos):
         if int(i['userId']) == int(argv[1]):
             if i['completed'] is True:
                 done.append(i['title'])
                 NUMBER_OF_DONE_TASKS += 1
             TOTAL_NUMBER_OF_TASKS += 1
 
-    print("Employee {} is done with tasks".format(EMPLOYEE_NAME), end='')
-    print('({}/'.format(NUMBER_OF_DONE_TASKS), end='')
-    print('{}):'.format(TOTAL_NUMBER_OF_TASKS), end='\n\t ')
-    print('\n\t '.join(done))
+    print("Employee {} is done with tasks({}/{}):"
+          .format(u["name"], len(done), len(todos)))
+    print("\n".join("\t {}".format(j) for j in done))
