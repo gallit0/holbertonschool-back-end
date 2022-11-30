883a978c6be56a21068c7e22b20b5d88f0b0384f
gallit0 <mateogesede@gmail.com>
2022-11-30 18:43:13 -0300

    task 1 fixed

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """API"""
+import csv
 import requests
 from sys import argv
-import csv
 
 src = 'https://jsonplaceholder.typicode.com/'
 
 if __name__ == '__main__':
     todos = requests.get(src + "todos?userId=" + argv[1]).json()
     u = requests.get(src + 'users/' + argv[1]).json()
-    EMPLOYEE_NAME = u['name']
+    uname = u['username']
     done = []
     j = 0
     for i in list(todos):
         if int(i['userId']) == int(argv[1]):
-            done.append([argv[1], u['name'], i['completed'], i['title']])
+            done.append([argv[1], uname, i['completed'], i['title']])
 
     with open('2.csv', 'w') as csvfile:
         sw = csv.writer(csvfile, delimiter=',', lineterminator='\r\n',
