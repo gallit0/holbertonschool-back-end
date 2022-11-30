059c1fcec1e16af46d6a2c986ec65b1cf5a1ba67
gallit0 <mateogesede@gmail.com>
2022-11-30 15:44:33 -0300

    import alphabetically

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """API"""
-import requests
 from sys import argv
 import json
+import requests
 
 if __name__ == '__main__':
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
