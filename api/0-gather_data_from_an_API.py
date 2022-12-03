d87b0a34df0d788b104132fa2236d23932494e9c
gallit0 <mateogesede@gmail.com>
2022-12-02 22:54:02 -0300

    attempt at checker with anothers peer's file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+made by mateo bonino
 """
 import requests
 import sys
